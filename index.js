7b07ab59e7081ec2dc5e272521210c05cd8d7ca9
Al-Ferdous <alferdous602@gmail.com>
2024-03-20 20:23:21 +0600

    added redux  payload

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
-const INCREMENT = "INCREMENT";
-const DECREMENT = "DECREMENT";
-const ADD_USER = "ADD_USER";
+@@ -1,5 +1,6 @@
+const { createStore } = require("redux")
+const INCREMENT = 'INCREMENT'
+const INCREMENT_BY_VALUE = 'INCREMENT_BY_VALUE'
+const DECREMENT = 'DECREMENT'
+const RESET = 'RESET'
 
-const decrementCounter = () => {
+@@ -24,11 +25,19 @@ const incrementCounterAction = () => {
+
+    }
+}
+const incrementCounterActionByValue = (value) => {
     return {
-        type: DECREMENT
+        type: INCREMENT_BY_VALUE,
+        payload: value,
+
     }
 }
 
-// create reducer for counter 
-const counterReducer = (state = { count: 0 }, action) => {
-    switch (action.type) {
-        case INCREMENT:
+// Decrement Counter 
+const decrementCounterAction = () => {
+    return {
+        type: DECREMENT
+        type: DECREMENT,
+
+
+    }
+}
+@@ -50,6 +59,12 @@ const counterReducer = (state = initialState, action) => {
+                count: state.count + 1
+            }
+
+        case INCREMENT_BY_VALUE:
             return {
                 ...state,
-                count: state.count + 1
+                count: state.count + action.payload
             }
 
         case DECREMENT:
             return {
                 ...state,
-                count: state.count - 1
-            }
+@@ -77,7 +92,8 @@ store.subscribe(() => {
+store.dispatch(incrementCounterAction())
+store.dispatch(incrementCounterAction())
+store.dispatch(decrementCounterAction())
+store.dispatch(resetCounterAction())
+// store.dispatch(resetCounterAction())
+store.dispatch(incrementCounterActionByValue(5))
 
-        default:
-            return state
-    }
-}
 
-const addUser = () => {
-    return {
-        type: ADD_USER,
-        payload: { name: 'Niaj' }
-    }
-}
+/* 
