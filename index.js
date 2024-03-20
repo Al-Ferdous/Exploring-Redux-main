f16772298fa234b1494e2fba512bf4e7a21d79b8
Al-Ferdous <alferdous602@gmail.com>
2024-03-20 20:30:06 +0600

    redux  payload issues resolved

--- index.js
+++ index.js
@@ -1,51 +1,75 @@
-@@ -1,5 +1,6 @@
-const { createStore } = require("redux")
-const INCREMENT = 'INCREMENT'
-const INCREMENT_BY_VALUE = 'INCREMENT_BY_VALUE'
-const DECREMENT = 'DECREMENT'
-const RESET = 'RESET'
+const { createStore } = require("redux");
 
-@@ -24,11 +25,19 @@ const incrementCounterAction = () => {
+// Action Types
+const INCREMENT = 'INCREMENT';
+const INCREMENT_BY_VALUE = 'INCREMENT_BY_VALUE';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
 
-    }
-}
+// Action Creators
+const incrementCounterAction = () => {
+    return {
+        type: INCREMENT
+    };
+};
+
 const incrementCounterActionByValue = (value) => {
     return {
         type: INCREMENT_BY_VALUE,
-        payload: value,
+        payload: value
+    };
+};
 
-    }
-}
-
-// Decrement Counter 
 const decrementCounterAction = () => {
     return {
         type: DECREMENT
-        type: DECREMENT,
+    };
+};
 
+const resetCounterAction = () => {
+    return {
+        type: RESET
+    };
+};
 
-    }
-}
-@@ -50,6 +59,12 @@ const counterReducer = (state = initialState, action) => {
+// Reducer
+const initialState = {
+    count: 0
+};
+
+const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
                 count: state.count + 1
-            }
-
+            };
         case INCREMENT_BY_VALUE:
             return {
                 ...state,
                 count: state.count + action.payload
-            }
-
+            };
         case DECREMENT:
             return {
                 ...state,
-@@ -77,7 +92,8 @@ store.subscribe(() => {
-store.dispatch(incrementCounterAction())
-store.dispatch(incrementCounterAction())
-store.dispatch(decrementCounterAction())
-store.dispatch(resetCounterAction())
-// store.dispatch(resetCounterAction())
-store.dispatch(incrementCounterActionByValue(5))
+                count: state.count - 1
+            };
+        case RESET:
+            return {
+                ...state,
+                count: 0
+            };
+        default:
+            return state;
+    }
+};
 
+// Store
+const store = createStore(counterReducer);
 
-/* 
+// Dispatch Actions
+store.dispatch(incrementCounterAction());
+store.dispatch(incrementCounterAction());
+store.dispatch(decrementCounterAction());
+store.dispatch(resetCounterAction());
+store.dispatch(incrementCounterActionByValue(5));
